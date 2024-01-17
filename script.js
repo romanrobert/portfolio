1183807dad2e8483a07e2bacbdb05a4f52d08fc5
romanrobert <iromanrobert@gmail.com>
2024-01-17 10:34:09 +0200

    added background blobs

--- script.js
+++ script.js
@@ -9,14 +9,20 @@ const getRandomNumber = (min, max) => {
 const updateBlobPosition = () => {
   blobs.forEach((blob) => {
     blob.style.setProperty("--position-x", `${getRandomNumber(1, 100)}vw`);
-    blob.style.setProperty("--animate-to-x", `${getRandomNumber(1, 100)}vw`);
     blob.style.setProperty("--position-y", `${getRandomNumber(1, 20)}vh`);
+  });
+};
+
+const updateBlobMovement = () => {
+  blobs.forEach((blob) => {
     blob.style.setProperty("--animate-to-y", `${getRandomNumber(1, 50)}vh`);
+    blob.style.setProperty("--animate-to-x", `${getRandomNumber(1, 100)}vw`);
   });
 };
 
 updateBlobPosition();
-setInterval(updateBlobPosition, 20000);
+
+setInterval(updateBlobMovement, 20000);
 
 const mobileMenuButton = document.querySelector(".mobile-nav-btn");
 const mobileNavigation = document.querySelector(".nav-list");
